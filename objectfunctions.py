fa7901250f3545c9c1b4a7ff30a6af199db8f5a7
Kevin Tran <kevin.tech.tran@gmail.com>
2020-11-03 15:06:59 -0800

    first commit

---
+++ objectfunctions.py
@@ -0,0 +1,6 @@
+from Student import Student
+
+student1 = Student("Oscar", "Accounting", 3.1)           # putting finctions into classes will be used a lot
+student2 = Student("Phyllis", "Business", 3.8)
+
+print(student2.on_honor_roll())
